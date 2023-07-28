98d82988e4b62d3e4a3ed6dcb2ff7c111f24928f
Alejandro López Paz <alejo.lpaz@gmail.com>
2023-07-29 04:47:19 +1000

    Disable login test

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -1,6 +1,7 @@
-describe('Render Login', () => {
+/*describe('Render Login', () => {
   it('should render login form', () => {
     cy.visit('/login')
     cy.get('h1').should('contain', 'Sign in to your account')
   })
 })
+*/
